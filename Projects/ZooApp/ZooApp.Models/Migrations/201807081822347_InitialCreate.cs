7e8560b3042b8e852c9d8af6904f021d778b8d6d
fahim <fahimforhad496@gmail.com>
2018-07-09 00:24:01 +0600

    DataBase Migration

---
+++ Projects/ZooApp/ZooApp.Models/Migrations/201807081822347_InitialCreate.cs
@@ -0,0 +1,29 @@
+namespace ZooApp.Models.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Animals",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(),
+                        Food = c.String(),
+                        Origin = c.String(),
+                        Quantity = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Animals");
+        }
+    }
+}
