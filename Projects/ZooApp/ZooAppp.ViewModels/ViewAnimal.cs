e169b998113478a51b4b4a9de622dc7080e424e9
fahim <fahimforhad496@gmail.com>
2018-07-14 21:03:19 +0600

    InitialCommit

--- Projects/ZooApp/ZooAppp.ViewModels/ViewAnimal.cs
+++ Projects/ZooApp/ZooAppp.ViewModels/ViewAnimal.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ZooApp.Services
+namespace ZooAppp.ViewModels
 {
-    public class Class1
+    public class ViewAnimal
     {
     }
 }
